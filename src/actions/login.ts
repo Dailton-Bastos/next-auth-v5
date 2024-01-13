2ca67699f2edc7fae8aed999960990d9a8b61fba
Dailton Bastos <dailtonbastos@gmal.com>
2024-01-13 17:13:54 -0300

    feat: add middleware and login

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -1,6 +1,9 @@
 'use server'
 
+import { signIn } from '@/auth'
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes'
 import { loginSchema } from '@/schemas'
+import { AuthError } from 'next-auth'
 import * as z from 'zod'
 
 export const login = async (values: z.infer<typeof loginSchema>) => {
@@ -10,5 +13,25 @@ export const login = async (values: z.infer<typeof loginSchema>) => {
     return { error: 'Invalid fields!' }
   }
 
-  return { sucess: 'Email sent!' }
+  const { email, password } = validatedFields.data
+
+  try {
+    await signIn('credentials', {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    })
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return { error: 'Invalid credentials!' }
+
+        default:
+          return { error: 'Oops, something went wrong!' }
+      }
+    }
+
+    throw error
+  }
 }
---
+++ src/auth.config.ts
@@ -0,0 +1,29 @@
+import { getUserByEmail } from '@/data/user'
+import { loginSchema } from '@/schemas'
+import bcrypt from 'bcryptjs'
+import type { NextAuthConfig } from 'next-auth'
+import Credentials from 'next-auth/providers/credentials'
+
+export default {
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = loginSchema.safeParse(credentials)
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data
+
+          const user = await getUserByEmail(email)
+
+          if (!user || !user.password) return null
+
+          const passwordsMatch = await bcrypt.compare(password, user?.password)
+
+          if (passwordsMatch) return user
+        }
+
+        return null
+      },
+    }),
+  ],
+} satisfies NextAuthConfig
