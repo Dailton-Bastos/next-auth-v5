7740d29bac8c1d27c0061cccc663d3f858eea248
Dailton Bastos <dailtonbastos@gmal.com>
2024-01-24 22:45:03 -0300

    feat: add settings page

--- src/actions/new-verification.ts
+++ src/actions/new-verification.ts
@@ -2,6 +2,7 @@
 
 import { getUserByEmail } from '@/data/user'
 import { getVerificationTokenByToken } from '@/data/verificationToken'
+import { currentUser } from '@/lib/auth'
 import { db } from '@/lib/db'
 
 export const newVerification = async (token: string) => {
@@ -19,12 +20,16 @@ export const newVerification = async (token: string) => {
 
   const existingUser = await getUserByEmail(existingToken?.email)
 
-  if (!existingUser) {
+  const hasAuthenticateUser = await currentUser()
+
+  if (!existingUser && !hasAuthenticateUser) {
     return { error: 'Email does not exist!' }
   }
 
+  const id = hasAuthenticateUser ? hasAuthenticateUser.id : existingUser?.id
+
   await db.user.update({
-    where: { id: existingUser?.id },
+    where: { id },
     data: {
       emailVerified: new Date(),
       email: existingToken?.email,
--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -1,20 +1,233 @@
 'use client'
 
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
-import { logout } from '@/actions/logout'
+import { settings } from '@/actions/settings'
+import { FormError } from '@/components/form-error'
+import { FormSuccess } from '@/components/form-success'
+import { Button } from '@/components/ui/button'
+import { Card, CardHeader, CardContent } from '@/components/ui/card'
+import {
+  Form,
+  FormField,
+  FormControl,
+  FormItem,
+  FormLabel,
+  FormDescription,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { Switch } from '@/components/ui/switch'
+import { useCurrentUser } from '@/hooks/use-current-user'
+import { settingsSchema } from '@/schemas'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { UserRole } from '@prisma/client'
+import { useSession } from 'next-auth/react'
+import * as z from 'zod'
 
 const SettingsPage = () => {
-  const onClick = () => {
-    logout()
+  const [error, setError] = React.useState<string | undefined>()
+  const [success, setSuccess] = React.useState<string | undefined>()
+  const [isPending, startTransition] = React.useTransition()
+
+  const { update } = useSession()
+  const user = useCurrentUser()
+
+  const form = useForm<z.infer<typeof settingsSchema>>({
+    resolver: zodResolver(settingsSchema),
+    defaultValues: {
+      name: user?.name || undefined,
+      email: user?.email || undefined,
+      password: undefined,
+      newPassword: undefined,
+      role: user?.role || undefined,
+      isTwoFactorEnabled: user?.isTwoFactorEnabled || undefined,
+    },
+  })
+
+  const onSubmit = (values: z.infer<typeof settingsSchema>) => {
+    startTransition(() => {
+      settings(values)
+        .then((data) => {
+          if (data?.error) {
+            setError(data.error)
+          }
+
+          if (data?.success) {
+            update()
+            setSuccess(data.success)
+          }
+        })
+        .catch(() => setError('Oops! Something went wrong!'))
+    })
   }
 
   return (
-    <div className='bg-white p-10 rounded-xl'>
-      <button type='button' onClick={onClick}>
-        Sign out
-      </button>
-    </div>
+    <Card className='w-[600px]'>
+      <CardHeader>
+        <p className='text-2xl font-semibold text-center'>⚙️ Settings</p>
+      </CardHeader>
+
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
+            <div className='space-y-4'>
+              <FormField
+                control={form.control}
+                name='name'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder='John Doe'
+                        disabled={isPending}
+                      />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              {!user?.isOAuth && (
+                <React.Fragment>
+                  <FormField
+                    control={form.control}
+                    name='email'
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+
+                        <FormControl>
+                          <Input
+                            {...field}
+                            placeholder='john.doe@example.com'
+                            type='email'
+                            disabled={isPending}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name='password'
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+
+                        <FormControl>
+                          <Input
+                            {...field}
+                            placeholder='******'
+                            type='password'
+                            disabled={isPending}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name='newPassword'
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>New Password</FormLabel>
+
+                        <FormControl>
+                          <Input
+                            {...field}
+                            placeholder='******'
+                            type='password'
+                            disabled={isPending}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </React.Fragment>
+              )}
+
+              <FormField
+                control={form.control}
+                name='role'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Role</FormLabel>
+
+                    <Select
+                      disabled={isPending}
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder='Select a role' />
+                        </SelectTrigger>
+                      </FormControl>
+
+                      <SelectContent>
+                        <SelectItem value={UserRole.ADMIN}>Admin</SelectItem>
+                        <SelectItem value={UserRole.USER}>User</SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              {!user?.isOAuth && (
+                <FormField
+                  control={form.control}
+                  name='isTwoFactorEnabled'
+                  render={({ field }) => (
+                    <FormItem className='flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm'>
+                      <div className='space-y-0.5'>
+                        <FormLabel>Two Factor Authentication</FormLabel>
+                        <FormDescription>
+                          Enable two factor authentication for your account
+                        </FormDescription>
+                      </div>
+
+                      <FormControl>
+                        <Switch
+                          disabled={isPending}
+                          checked={field.value}
+                          onCheckedChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              )}
+            </div>
+
+            <FormError message={error} />
+            <FormSuccess message={success} />
+
+            <Button type='submit' disabled={isPending}>
+              Save
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
   )
 }
 
